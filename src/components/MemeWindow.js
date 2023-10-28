c8d6a1fd0754dd716eea17a3ed0fa714449898e3
notvitalik <abhbasu7@gmail.com>
2023-10-27 21:43:57 -0400

    added  mute to youtube video - sorry Saylor

--- src/components/MemeWindow.js
+++ src/components/MemeWindow.js
@@ -5,7 +5,7 @@ function MemeWindow() {
   return (
     <div className="meme-window">
       <Center><h3><b>MEMES</b></h3></Center>
-      <iframe src="https://www.youtube.com/embed/wIhTGB3wqV0?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/wIhTGB3wqV0?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
       {/* <video controls>
         <source src="path_to_your_video.mp4" type="video/mp4" />
